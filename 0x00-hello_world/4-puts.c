d83a46ae16f9911adaa746fa7d670eebba29edc2
sboniseni <siboniseni.kasa@outlook.com>
2023-02-16 04:49:56 -0800

    betty issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 int main(void) 
 {
 	puts("⧵"Programming is like building a multilingual puzzle");
-	  return (0);
+	return (0);
 }
 
 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - a C program that print a line using printf function
+*return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,⧵n")
+return (0);
+}
+
