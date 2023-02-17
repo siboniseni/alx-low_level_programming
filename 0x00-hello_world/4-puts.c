a4074ac40b778058992422f325978b78b8b2d3d8
sboniseni <siboniseni.kasa@outlook.com>
2023-02-16 20:45:45 -0800

    task 4. 5. 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (success)
- */
-int main(void) 
+* main - A program that print a  line with puts function
+* Return: 0 (success)
+*/
+int main(void)
 {
-	puts("⧵"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("⧵"Programming is like bulding a multilingual puzzle");
+	return 0;
 }
-
-
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - a C program that print a line using printf function
-*return: 0 (success)
- */
+* main - A program that prints a line using printf
+*Return: 0 (success)
+*/
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,⧵n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,⧵n")
+	Return(0);
 }
-
