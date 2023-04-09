d03e75beaa6f91f49306a807c83dcb18579e6596
siboniseni <siboniseni.kasa@outlook.com>
2023-04-09 01:04:11 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,9 +8,8 @@
   */
 
 unsigned int binary_to_uint(const char *b)
-{	
-
-	unsigned int base;
+{
+	unsigned int d = 0;
 
 	if (b == NULL)
 		return (0);
@@ -20,10 +19,11 @@ unsigned int binary_to_uint(const char *b)
 		if (*b < '0' || *b > '1')
 			return (0);
 
-		base = base * 2 + *b - '0';
-		b++
+		d = d * 2 + *b - '0';
+		b++;
 	}
 
-	return (base)
+	return (d);
 
 }
+
