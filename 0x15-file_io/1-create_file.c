55bcafb9780889dfe0a1b630cc453ed4139a9faa
Siboniseni Kasa <122732898+siboniseni@users.noreply.github.com>
2023-05-09 10:30:08 +0200

    Update 1-create_file.c and comments

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,30 +1,46 @@
 #include "main.h"
+/**
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+int _strlen(char *s)
+{
+	int i = 0;
 
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+* create_file - Create a function tha creates a file.
+* @filename: file to create.
+* @text_content: info to write into the file.
+* Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t nletters;
+	int file;
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1;
-    }
-
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-        return -1;
-    }
-
-    if (text_content != NULL) {
-        ssize_t len = strlen(text_content);
-        ssize_t bytes_written = write(fd, text_content, len);
-        if (bytes_written != len) {
-            close(fd);
-            return -1;
-        }
-    }
-
-    close(fd);
-    return 1;
+	if (!filename)
+		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
